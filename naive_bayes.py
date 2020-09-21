4f44d622a8097977df8b034ed24c037d277137e6
fadilparves <fadil.parves@gmail.com>
2020-09-21 16:04:08 +0800

    added fit and init method in class

---
+++ naive_bayes.py
@@ -0,0 +1,32 @@
+import numpy as np
+import math
+
+class MultinomialNaiveBayes:
+
+    def __init__(self, classes, tokenizer):
+        self.tokenizer = tokenizer
+        self.classes = classes
+
+    
+    
+    def fit(self, X, y):
+        self.n_class_items = {}
+        self.log_class_priors = {}
+        self.word_counts = {}
+        self.vocab = set()
+
+        n = len(X)
+        grouped_data = self.group_by_class(X, y)
+
+        for c, data in grouped_data.items():
+            self.n_class_items[c] = len(data)
+            self.log_class_priors[c] = math.log(self.n_class_items[c] / n)
+            self.word_counts[c] = defaultdict(lambda: 0)
+
+            for text in data:
+                counts = Counter(self.tokenizer.tokenize(text))
+                for word, count in counts.item():
+                    if word no in self.vocab:
+                        self.vocab.add(word)
+                    
+                    self.word_counts[c][word] += count
