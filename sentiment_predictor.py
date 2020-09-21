f323c1d5d6b163eb2fbbcc7b8701273459e62d6a
fadilparves <fadil.parves@gmail.com>
2020-09-21 16:46:11 +0800

    added tokenizer class file

--- sentiment_predictor.py
+++ sentiment_predictor.py
@@ -1,18 +1,14 @@
 import pandas as pd
 import numpy as np
 import seaborn as sns
-import string
-import re
 import matplotlib.pyplot as plt
 from matplotlib import rc
 from sklearn.model_selection import train_test_split
-from bs4 import BeautifulSoup
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
-import nltk
-from nltk.corpus import stopwords
 from wordcloud import WordCloud
 from naive_bayes import MultinomialNaiveBayes
 from pylab import rcParams
+from nltk.corpus import stopwords
 
 sns.set(style='whitegrid', palette='muted')
 rcParams['figure.figsize'] = 14,8
@@ -29,3 +25,4 @@ plt.figure()
 plt.imshow(wordcloud, interpolation='bilinear')
 plt.axis("off")
 plt.savefig("./output/wordcloud.png")
+
---
+++ tokenizer.py
@@ -0,0 +1,6 @@
+import string
+import re
+from bs4 import BeautifulSoup
+import nltk
+from nltk.corpus import stopwords
+
