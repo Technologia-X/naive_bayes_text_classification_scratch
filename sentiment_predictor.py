5c725774bdc78615b9dfd700a083bd0fe237e901
fadilparves <fadil.parves@gmail.com>
2020-09-21 16:49:27 +0800

    added tokenizer class done

--- sentiment_predictor.py
+++ sentiment_predictor.py
@@ -6,9 +6,11 @@ from matplotlib import rc
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 from wordcloud import WordCloud
-from naive_bayes import MultinomialNaiveBayes
 from pylab import rcParams
 from nltk.corpus import stopwords
+
+from naive_bayes import MultinomialNaiveBayes
+from tokenizer import Tokenizer
 
 sns.set(style='whitegrid', palette='muted')
 rcParams['figure.figsize'] = 14,8
